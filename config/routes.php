3bd97944838c5b5e1a2be326faee7366f52a194b
Peter Gassner <peter@naehrstoff.ch>
2008-11-20 17:49:27 +0100

    Change: show the ideas page without login requirements on the front page

--- config/routes.php
+++ config/routes.php
@@ -34,7 +34,7 @@
  * to use (in this case, /app/views/pages/home.thtml)...
  */
 	//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-	Router::connect('/', array('controller' => 'ideas', 'action' => 'home', 'admin' => true));
+	Router::connect('/', array('controller' => 'ideas', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
