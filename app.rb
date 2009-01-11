4a068d6abd670c0868a62f6b20d63304caf71b17
Wiederkehr <Benji@Kiowa.local>
2009-01-11 20:18:00 +0100

    added peters files to the master

---
+++ app.rb
@@ -0,0 +1,48 @@
+# run with `ruby app.rb`
+
+require 'rubygems'
+require 'sinatra'
+require 'geokit'
+require 'yahoo-weather'
+
+include Geokit::Geocoders
+Geokit::default_units = :kilometers
+
+get '/' do
+  erb :index
+end
+
+get '/create' do
+  erb :create
+end
+
+get '/location' do
+  peter = YahooGeocoder.geocode 'Zentralstrasse 118 Wettingen Schweiz'
+  benji = YahooGeocoder.geocode 'Rathausgässli 31 Lenzburg Schweiz'
+  haml "%p Von Peter zu Benji sind es #{peter.distance_to(benji).round} Kilometer"
+end
+
+get '/weather' do
+  response = YahooWeather::Client.new.lookup_location("SZXX0033", 'c')
+  [response.title, response.condition.temp, response.condition.text].join("\n")
+end
+
+not_found do
+  "Nüüt gits!"
+end
+
+error do
+  "Pfui, bäh, en Fehler!"
+end
+
+helpers do
+  def l(title, path = nil, attributes = {})
+    attributes[:href]  = path ||= title.downcase
+    attributes[:class] = "active" if request.path_info =~ /#{path}$/i
+    "<a #{attributes.map{|a,v| "#{a}='#{v}'"}.join(" ")}>#{title}</a>"
+  end
+  
+  def to_attr(hash)
+    attributes.map{|a,v| "#{a}='#{v}'"}.join(" ")
+  end
+end
---
+++ public/javascripts/application.js
@@ -0,0 +1,99 @@
+/* Global variables
+-----------------------------------------*/
+var FILLER = "______";
+
+
+/* Helper functions
+-----------------------------------------*/
+function isNumeric(val) {
+  var RE = /^-{0,1}\d*\.{0,1}\d+$/;
+  return RE.test(val);
+}
+
+function formSave(value, settings) { 
+  $("#number_of_results").html(parseInt(Math.random() * 200));
+  $.cookie($(this).attr("rel"), value, { expires: 14 });
+  return(value);
+}
+
+
+/* Run when document is ready
+-----------------------------------------*/
+$(document).ready(function() {
+  // Prepare edit fields
+  $('.edit').each(function() {
+    var name = $(this).attr("rel");
+    if ($.cookie(name)) {
+      $(this).html($.cookie(name));
+    } else {
+      $(this).html(FILLER);
+    }
+  });
+  
+  $('#clear_cookies').click(function() {
+    $("[rel]").each(function() {
+      $.cookie($(this).attr("rel"), null);
+    });
+  });
+});
+
+
+/* hook up the forms
+-----------------------------------------*/
+$(document).ready(function() {
+  // Form for numbers
+  $('[rel=number_of_persons], [rel=amount_of_money]').editable(formSave, { 
+    tooltip : "Move mouseover to edit...",
+    event   : "click",
+    onblur  : "submit",
+    style   : "inherit",
+    type    : "number"
+  });
+
+  // Form for amount of money
+  $("[rel=date]").editable(formSave, {
+    event  : "click",
+    delay  : 1500,
+    data   : dateList,
+    type   : "select",
+    onblur : "submit",
+    style  : "inherit"
+  }).change(function() {
+    $('form', this).submit();
+  });
+  
+  // Form for the available time
+  $("[rel=time]").editable(formSave, {
+    event  : "click",
+    delay  : 1500,
+    data   : timeList,
+    type   : "select",
+    onblur : "submit",
+    style  : "inherit"
+  }).change(function() {
+    $('form', this).submit();
+  });
+});
+
+
+/* jeditable input types
+-----------------------------------------*/
+$.editable.addInputType("number", {
+  element : function(settings, original) {
+    var input = $('<input type="text">');
+    $(this).append(input);
+    return(input);
+  },
+  submit: function (settings, original) {
+    var value = $("input", this).val();
+    if (isNumeric(value)) {
+      $("input", this).val(value);
+    } else {
+      $("input", this).val(FILLER);
+    }
+  },
+  content : function(string, settings, original) {
+    var value = (isNumeric(string)) ? string : "";
+    $("input", this).val(value);
+  }
+});
